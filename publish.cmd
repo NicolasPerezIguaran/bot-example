248f3d416f2b9b549c04418e7f2867c0d2f00071
Nicolás Pérez Iguarán <n.pereziguaran@gmail.com>
2019-06-24 18:56:47 +0200

    Add files

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild CoreBot.sln -p:DeployOnBuild=true -p:PublishProfile=bot0ejemplonpi-Web-Deploy.pubxml -p:Password=LCNhpjCGqR1bceuQWyGa9ejnWytKlR4SxTzt48az7bpRkBlcxKxJg96YpYQl
+
